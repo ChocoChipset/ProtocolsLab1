0bec4e51b987227fcb9c5fd1fafa541bf352d9ef
Hector Zarate <hector@spotify.com>
2016-05-05 14:47:30 +0200

    Initial Commit

---
+++ ProtocolsLab1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtocolsLab1
+//
+//  Created by Hector Zarate on 5/5/16.
+//  Copyright © 2016 iOSCowboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProtocolsLab1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ProtocolsLab1
+//
+//  Created by Hector Zarate on 5/5/16.
+//  Copyright © 2016 iOSCowboy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
